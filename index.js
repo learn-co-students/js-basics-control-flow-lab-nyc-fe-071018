4d321f87fdd8bd285aa247d26b920cad38db7ae9
David Gewirtzman <dgewirtzman@playbill.com>
2018-08-10 00:37:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code in this file!
+// rideLength = 199;
+
+function scuberGreetingForFeet(someValue) {
+  if (someValue < 400) {
+    return 'This one is on me!';
+  }
+  if (someValue > 2000 && someValue < 2501) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  if (someValue > 2500) {
+    return 'No can do.';
+  }
+}
+
+function ternaryCheckCity(someValue) {
+  return (someValue === 'NYC' ? 'Ok, sounds good.' : 'No go.');
+}
+
+function switchOnCharmFromTip(someValue) {
+  switch (someValue) {
+    case 'generous':
+    return 'Thank you so much.';
+    break;
+    case 'not as generous':
+    return 'Thank you.';
+    break;
+    default: 
+    return 'Bye.';
+  }
+}
